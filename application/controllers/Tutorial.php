c257932722c18d7bb85b701cba9e6433b4712a2c
ssol_ <sucuvu@gmail.com>
2021-01-19 23:53:32 +0900

    login 기능 추가 중

--- application/controllers/Tutorial.php
+++ application/controllers/Tutorial.php
@@ -3,14 +3,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Tutorial extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
 	public function index()
 	{
-		echo "Hello Index";
+		$result = $this->db->query('select * from member')->result();
+		foreach ($result as $item) {
+			echo $item->name.'  ';
+			echo $item->id.'  ';
+		}
 	}
 
 	public function second()
 	{
-		echo "Hello Seconddd";
+		echo "Hello Second";
 	}
 }
 ?>
---
+++ application/models/Auth_m.php
@@ -0,0 +1,32 @@
+<?php
+
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class auth_m extends CI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * 아이디 비밀번호 체크
+	 *
+	 * @param array $auth 폼 전송받은 아이디, 비밀번호
+	 * @return array
+	 */
+	function login($auth)
+	{
+		$sql = "SELECT id FROM member WHERE usernaWme = '" . $auth['id'] . "' AND password = '" . $auth['pwd'] . "' ";
+
+		$query = $this->db->query($sql);
+
+		if ($query->num_rows() > 0) {
+			return $query->row();
+		} else {
+			return FALSE;
+		}
+	}
+
+}
